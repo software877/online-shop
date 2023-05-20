6b15c9a2dd2bba185d1f0d48088db4d059390d68
atarasov <tarasovaleksandr59@gmail.com>
2023-05-20 08:50:20 +0300

    compose has been added

--- app/src/main/java/com/example/onlineshop/MainActivity.kt
+++ app/src/main/java/com/example/onlineshop/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.example.onlineshop
 
 import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
 
     var navHostFragment: NavHostFragment? = null
     var navController: NavController? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContent {
+
+            Column(modifier = Modifier.fillMaxSize()) {
+                Button(modifier = Modifier.fillMaxWidth(), onClick = { println("This is click!!!") }) {
+                    Text(text = "Base Button!!!!")
+                }
+            }
+
+        }
+
+/*        setContentView(R.layout.activity_main)
 
         navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController = navHostFragment?.navController
+        navController = navHostFragment?.navController*/
     }
 
     fun openSecondPage() {
---
+++ app/src/main/java/com/example/onlineshop/screens/login_screen/LoginScreen.kt
@@ -0,0 +1,8 @@
+package com.example.onlineshop.screens.login_screen
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun LoginScreen() {
+
+}
